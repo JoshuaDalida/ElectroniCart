b1e4066b59000048d381496cfa6e214733ca2efb
Omar Abou-Saleh <omar@insightedge.dev>
2024-04-14 21:44:36 -0600

    Add nav with broke things....

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -3,13 +3,22 @@ import Link from 'next/link';
 
 const Product = ({ product }) => {
   return (
-    <div className="m-3">
+    <div className="m-3 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 overflow-hidden">
       <Link href={`product/${product.id}`}>
-        {product.imageURL && <img src={product.imageURL} alt={product.name} />}
-        <h2 className="text-gray-600">{product.name}</h2>
-        <p className="text-gray-600">Price: ${product.price}</p>
+        {product.imageURL && (
+          <img
+            src={product.imageURL}
+            alt={product.name}
+            className="w-full h-48 object-cover transition-transform duration-300 hover:scale-105"
+          />
+        )}
+        <div className="p-4">
+          <h2 className="text-lg font-semibold text-gray-800">{product.name}</h2>
+          <p className="text-gray-700">Price: ${product.price}</p>
+        </div>
       </Link>
     </div>
+
   );
 };
 
--- app/profile/wrapper/pageWrapper.js
+++ app/profile/wrapper/pageWrapper.js
@@ -84,7 +84,6 @@ export default function Profile() {
                 </form>
             </div>
         </div>
-
     );
 }
 
