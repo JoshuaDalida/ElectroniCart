d814aff0da448b65d68b62bcf84025c0d6050ed5
Omar Abou-Saleh <omar@insightedge.dev>
2024-04-16 00:51:14 -0600

    Added update and delete fuctionality to the profile page

---
+++ app/api/profile/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+
+export async function PUT(req: Request){
+    const{ name, email, gender, description } = await req.json();
+    const prisma = new PrismaClient();
+    console.log(name, email);
+    try {
+        await prisma.user.upsert({
+            where: { email: email},
+            update: { name, email, gender, description },
+            create: { name, email, gender, description },
+        });
+        console.log("Profile updated successfully!");
+    } catch (error) {
+        console.error("Failed to update profile.", error);
+    }
+    return new NextResponse('PUT /profile')
+}
+
+export async function DELETE(req: Request){
+    console.log("block 1");
+    const { email } = await req.json();
+    const prisma = new PrismaClient();
+    try {
+        await prisma.user.delete({
+            where: { email },
+        });
+        console.log("Profile deleted successfully!");
+    } catch (error) {
+        console.error("Failed to delete profile.", error);
+    }
+    return new NextResponse('DELETE /profile')
+}
